53dcfb43a9fc2c5df059e516f6f99349f9895ef2
komaba <43165861+komaba@users.noreply.github.com>
2019-01-22 11:42:43 +0900

    change

--- main.ts
+++ main.ts
@@ -3,31 +3,14 @@
  */
 //% weight=2 color=#0089f1 icon="\uf287"
 namespace web {
-    
-    /**
-     * Print a line of text to the serial port
-     * @param value to send over serial
-     */
-    //% blockId=serial_writeline block="serial|write line %text"
-    //% text.shadowOptions.toString=true
-    function writeLine(text: string): void {
-        if (!text) text = "";
-        // pad data to the 32 byte boundary
-        // to ensure apps receive the packet
-        let r = (32 - (text.length + 2) % 32) % 32;
-        serial.writeString(text);
-        for (let i = 0; i < r; ++i)
-            serial.writeString(" ");
-        serial.writeString("\r\n");
-    }
     /**
      * Write a name:value pair as a line to the serial port.
      * @param name name of the value stream, eg: x
      * @param value to write
      */
-    //% blockId=serial_writevalue block="serial|write value %name|= %value"
+    //% blockId=serial_writevalue block="send value %name|= %value"
     export function writeValue(name: string, value: number): void {
-        writeLine("{\"t:" + input.runningTime() + ",\"s\":" + control.deviceSerialNumber() + ",\"n\":\"" + name + "\",\"v\":" + value + "}");
+        serial.writeLine("{\"t:" + input.runningTime() + ",\"s\":" + control.deviceSerialNumber() + ",\"n\":\"" + name + "\",\"v\":" + value + "}");
     }
 
 }
